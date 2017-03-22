88322c8884fe405db071ef6c274e74f77ee66d0f
swc03 <swc03@DESKTOP-ENC52E2>
2017-03-22 13:37:41 +0900

    실습 과제1

--- JavaPractice/src/org/dimigo/basic/PrimitiveDataType.java
+++ JavaPractice/src/org/dimigo/basic/PrimitiveDataType.java
@@ -18,8 +18,28 @@ public class PrimitiveDataType {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//
+		
+		String name = "아이유";
+		boolean isMale = false;
+		int age = 25;
+		double height = 161.8;
+		float weight = 44.3f;
+		char bloodType = 'A';
+		
+		System.out.println("<<아이유 프로필>>");
+		System.out.println("이름: "+name);
+		if(isMale==true)
+		{
+		System.out.println("성별: 남자");
+		}
+		else
+		{
+			System.out.println("성별: 여자");
+		}
+		System.out.println("나이: "+age+"세");
+		System.out.println("키: "+height+"cm");
+		System.out.println("몸무게: "+weight+"kg");
+		System.out.println("혈액형: "+bloodType+"형");
 	}
 
 }
