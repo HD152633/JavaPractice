61130356edda92bbb75700c2c3234554a98484e7
swc03 <swc03@DESKTOP-ENC52E2>
2017-03-23 13:39:32 +0900

    실습과제2

---
+++ JavaPractice/src/org/dimigo/basic/Operator.java
@@ -0,0 +1,26 @@
+package org.dimigo.basic;
+
+/**
+ * <pre>
+ * org.dimigo.basic
+ *	 |_ Operator
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 3. 23.
+ * </pre>
+ *
+ *	@author      : swc03
+ *	@version 	 : 1.0
+ */
+public class Operator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int price = 1500;
+		System.out.println(String.format("%,d", price));
+
+	}
+
+}
