8f0c55ab45b6da69d3dd7f3258909ed017a98e57
swc03 <swc03@DESKTOP-ENC52E2>
2017-03-27 12:33:44 +0900

    실습과제들 입니다.

--- JavaPractice/src/org/dimigo/basic/Operator.java
+++ JavaPractice/src/org/dimigo/basic/Operator.java
@@ -18,8 +18,17 @@ public class Operator {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int price = 1500;
-		System.out.println(String.format("%,d", price));
+		
+		int price = 1700000;
+		int person = 3;
+		int num = 1500;
+		long res = (long)12*price*person*num;
+		
+		System.out.println("<<디미베네 연간 인건비>>");
+		System.out.println("월 평균 급여 : "+String.format("%,d", price)+'원');
+		System.out.println("점포 내 직원 수 : "+person+'명');
+		System.out.println("점포 수 : "+String.format("%,d", num)+'개');
+		System.out.println("\n연간 인건비 : "+String.format("%,d", res)+'원');
 
 	}
 
