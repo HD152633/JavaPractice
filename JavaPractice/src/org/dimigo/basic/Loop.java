e469a95e2a3e49fe063fe87dd0310d2a7ee2d1a8
swc03 <swc03@DESKTOP-ENC52E2>
2017-04-17 13:33:31 +0900

    실습과제입니다.

--- JavaPractice/src/org/dimigo/basic/Loop.java
+++ JavaPractice/src/org/dimigo/basic/Loop.java
@@ -1,5 +1,6 @@
 package org.dimigo.basic;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -22,7 +23,8 @@ public class Loop {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-		
+		int attack = 100;
+		String[] b ={"마법사", "영주", "기사", "농민"};
 		while(true){
 					
 	    System.out.println("--------------------");
@@ -32,11 +34,29 @@ public class Loop {
 		System.out.println("3. 캐릭터 설정");
 		System.out.println("9. 종료");
 		System.out.println("--------------------");
-		System.out.println("");	
-			int choice = scanner.nextInt();
-			System.out.println(choice);
-			//if(choice==)
-			if(choice==9) break;
+		System.out.print("메뉴 입력 => ");	
+			
+		int choice = scanner.nextInt();
+			
+		if(choice==1){
+				attack+=10;
+				System.out.println("공격력이 증가되었습니다.현재 공격력 : "+attack);
+					}
+			
+		else if(choice==2){
+				attack-=10;
+				System.out.println("공격력이 감소되었습니다.현재 공격력 : "+attack);
+					}
+		else if(choice==3){
+				int a = new Random().nextInt(4);
+				System.out.println(b[a]+"(으)로 설정되었습니다.");
+					}
+		else if(choice==9){
+				System.out.println("이제 공부하세요!");
+				break;
+					}
+		else System.out.println("없는 메뉴입니다!!");
+		
 		}
 	}
 
